2337856734d5d9baea074cb54eabd930a04477b7
John Sutherland <john@sneeu.com>
2008-12-15 14:37:14 +0000

    Removes debugging from the JS.

--- slides.js
+++ slides.js
@@ -4,12 +4,9 @@ $(function () {
 	function nextSlide() {
 		var next = current.next();
 		if (next.length == 0) {
-			console.log('Next = first');
 			var prev = current.prevAll();
 			next = $(prev[prev.length - 1]);
 		}
-
-		console.log(current, next);
 
 		current.css('left', '0').animate({'left': '-100%'}, 'slow');
 		next.css('display', 'block').css('top', '0').css('left', $('html').css('width')).animate({'left': '0'}, 'slow');
